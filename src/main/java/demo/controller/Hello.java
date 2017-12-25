48af08b5149261567c32c34cc70f602772b35ab3
Latinist <antonromanov@list.ru>
2017-12-25 18:55:20 +0300

    first upload

---
+++ src/main/java/demo/controller/Hello.java
@@ -0,0 +1,19 @@
+package demo.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class Hello {
+	
+	
+	@RequestMapping("/")
+    public String sayHello() {
+        return "Hello Spring Boot!!";
+    }	
+	
+
+}
